a23a27f1d5fa41cbd7e31c16c63924fbf5e6f809
joesabbagh1 <joe.sabbagh2001@gmail.com>
2023-01-22 18:01:09 +0200

    add favorite shop working

--- src/components/CoffeeShop.tsx
+++ src/components/CoffeeShop.tsx
@@ -1,12 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar, faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons'
 import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons'
-import React, { useMemo, FC, useState } from 'react'
+import React, { useMemo, FC, useState, useEffect } from 'react'
 import { Shop } from '@prisma/client'
 import Image from 'next/image'
 import younes from './images/younes.jpeg'
-import { router } from '../server/trpc/trpc'
 import Link from 'next/link'
+import { trpc } from '../utils/trpc'
+import { useSession } from 'next-auth/react'
 
 type getIcon = (idx: number) => any
 
@@ -14,7 +15,19 @@ type coffeeShop = {
   coffeeShop: Shop
 }
 const CoffeeShop: FC<coffeeShop> = ({ coffeeShop }) => {
+  const session = useSession()
+
+  const favShop = trpc.favorite.getById.useQuery({
+    userId: session.data?.user?.id,
+    shopId: coffeeShop.id,
+  })
+
   const [favorite, setFavorite] = useState(false)
+  useEffect(() => {
+    if (favShop.status === 'success') {
+      setFavorite(favShop.data?.length !== 0 ? true : false)
+    }
+  }, [favShop.data])
 
   const getIcon: getIcon = (idx) => {
     if (idx <= coffeeShop.rating) {
@@ -37,15 +50,32 @@ const CoffeeShop: FC<coffeeShop> = ({ coffeeShop }) => {
       .map((idx) => <FontAwesomeIcon key={idx} icon={getIcon(idx)} style={{ color: '#f5eb3b' }} />)
   }, [coffeeShop.rating, getIcon])
 
-  function favShop() {
+  const createMutation = trpc.favorite.createFavorite.useMutation()
+
+  async function handleCreation(data: any) {
+    await createMutation.mutateAsync(data)
+  }
+
+  const deleteMutation = trpc.favorite.deleteFavorite.useMutation()
+
+  async function handleDeletion(data: any) {
+    await deleteMutation.mutateAsync(data)
+  }
+
+  async function handleFavShop() {
     setFavorite(!favorite)
+    if (!favorite) {
+      handleCreation({ userId: session.data?.user?.id, shopId: coffeeShop.id })
+    } else {
+      handleDeletion({ userId: session.data?.user?.id, shopId: coffeeShop.id })
+    }
   }
 
   return (
     <div className="relative">
       <button
-        className="absolute left-auto right-3 top-3 z-50 cursor-pointer"
-        onClick={() => favShop()}
+        className="absolute left-auto right-3 top-3 z-50 scale-110 cursor-pointer"
+        onClick={() => handleFavShop()}
       >
         <FontAwesomeIcon
           className="rounded-full bg-white p-2"
@@ -54,7 +84,7 @@ const CoffeeShop: FC<coffeeShop> = ({ coffeeShop }) => {
         />
       </button>
       <Link href={{ pathname: `/shop/${coffeeShop.id}` }}>
-        <div className="ransition cursor-pointer flex-col gap-3 decoration-slate-800 duration-100 ease-in-out hover:bg-white hover:opacity-80">
+        <div className="cursor-pointer flex-col gap-3 transition duration-200 ease-in-out hover:bg-white hover:opacity-80">
           <Image src={younes} alt="" />
           <div className="mt-3 text-lg font-semibold decoration-8">{coffeeShop.title}</div>
           <span className="text-lg font-light">Price: {getPrice()}</span>
--- src/server/trpc/router/favShop.ts
+++ src/server/trpc/router/favShop.ts
@@ -3,10 +3,11 @@ import { z } from "zod";
 import { router, publicProcedure } from "../trpc";
 
 export const favoriteRouter = router({        
-  getById: publicProcedure.input(z.string()).query(async ({ ctx, input }) => {
-    return await ctx.prisma.favorite.findFirst({
+  getById: publicProcedure.input(z.any()).query(async ({ ctx, input }) => {
+    return await ctx.prisma.favorite.findMany({
       where:{
-        id: input
+        userId: input.userId,
+        shopId: input.shopId
       }
     });
   }),
@@ -17,5 +18,13 @@ export const favoriteRouter = router({
     await ctx.prisma.favorite.create({
       data: input
      })
+  }),
+  deleteFavorite: publicProcedure.input(z.any()).mutation(async ({ctx, input})=>{
+    await ctx.prisma.favorite.deleteMany({
+      where:{
+        userId: input.userId,
+        shopId: input.shopId
+      }
+    })
   })
 });
